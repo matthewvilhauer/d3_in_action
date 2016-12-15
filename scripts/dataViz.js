a8cb0161622d69870b8659f009c9eeb7b8698843
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-12-15 11:03:01 -0800

    Adds basic histogram for newly created nested object that was made from tweets.json

--- scripts/dataViz.js
+++ scripts/dataViz.js
@@ -1,14 +1,14 @@
-d3.csv("../data/cities.csv", function(error, data) {dataViz(data);});
+d3.csv("../data/cities.csv", function(error, data) {csvDataViz(data);});
 
-function dataViz(incomingData) {
+function csvDataViz(incomingData) {
     var maxPopulation = d3.max(incomingData, function(el) {
        return parseInt(el.population);
     });
 
     var yScale = d3.scale.linear().domain([0,maxPopulation]).range([0,460]);
 
-    d3.select("svg").attr("style", "height: 480px; width: 600px");
-    d3.select("svg")
+    d3.select("svg").attr("style", "height: 480px; width: 600px; border: 1px solid #F2F2F2");
+    d3.select("svg.cities-csv-histogram")
         .selectAll("rect")
         .data(incomingData)
         .enter()
@@ -23,6 +23,36 @@ function dataViz(incomingData) {
         .style("opacity", .25)
 }
 
+d3.json("/data/tweets.json", function(error, data) {jsonDataViz(data.tweets)});
 
 
+function jsonDataViz(incomingData) {
+
+    var nestedTweets = d3.nest()
+        .key(function(el) {return el.user})
+        .entries(incomingData);
+
+    nestedTweets.forEach(function(el) {
+        el.numTweets = el.values.length;
+    });
+
+    var maxTweets = d3.max(nestedTweets, function(el) {return el.numTweets;});
+
+    var yScale = d3.scale.linear().domain([0,maxTweets]).range([0,480]);
+
+    d3.select("svg.tweets-json-histogram").attr("style", "height: 480px; width: 600px; border: 1px solid #F2F2F2");
+    d3.select("svg.tweets-json-histogram")
+        .selectAll("rect")
+        .data(nestedTweets)
+        .enter()
+        .append("rect")
+        .attr("width", 50)
+        .attr("height", function(d) {return yScale(parseInt(d.numTweets));})
+        .attr("x", function(d,i) {return i * 60;})
+        .attr("y", function(d) {return 480 - yScale(d.numTweets);})
+        .style("fill", "red")
+        .style("stroke", "blue")
+        .style("stroke-width", "1px")
+        .style("opacity", .25)
+}
 
